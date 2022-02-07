ece36f2cdc953ed079ca873673768f521c107c02
Michael Marlowe <marlowemich@gmail.com>
2022-02-07 15:10:45 -0500

    data for five day forecast now appearing

--- script.js
+++ script.js
@@ -25,6 +25,16 @@ var day1temp = document.getElementById("temp1");
 var day1wind = document.getElementById("wind1");
 var day1hum = document.getElementById("hum1");
 var day1uvi = document.getElementById("uvi1");
+var fiveday = [
+    document.getElementById("0"), 
+    document.getElementById("1"), 
+    document.getElementById("2"), 
+    document.getElementById("3"), 
+    document.getElementById("4")
+]
+var temp = document.getElementById("temp");
+var wind = document.getElementById("wind");
+var hum = document.getElementById("hum");
 
 btn.addEventListener("click", getAPI);
 
@@ -56,6 +66,35 @@ function getAPI() {
           day1hum.textContent = "Humidity: " + data.current.humidity + "%";
           day1uvi.textContent = "UV Index: " + data.current.uvi;
           day1wind.textContent = "Wind speed: " + data.current.wind_speed + "MPH";
+
+
+          fiveday[0].children[0].textContent = data.daily[0].temp.day + "\xB0F";
+          fiveday[0].children[1].textContent = data.daily[0].wind_speed + "MPH";
+          fiveday[0].children[2].textContent = data.daily[0].humidity + "%";
+
+          fiveday[1].children[0].textContent = data.daily[1].temp.day + "\xB0F";
+          fiveday[1].children[1].textContent = data.daily[1].wind_speed + "MPH";
+          fiveday[1].children[2].textContent = data.daily[1].humidity + "%";
+
+          fiveday[2].children[0].textContent = data.daily[2].temp.day + "\xB0F";
+          fiveday[2].children[1].textContent = data.daily[2].wind_speed + "MPH";
+          fiveday[2].children[2].textContent = data.daily[2].humidity + "%";
+          
+          fiveday[3].children[0].textContent = data.daily[3].temp.day + "\xB0F";
+          fiveday[3].children[1].textContent = data.daily[3].wind_speed + "MPH";
+          fiveday[3].children[2].textContent = data.daily[3].humidity + "%";
+
+          fiveday[4].children[0].textContent = data.daily[4].temp.day + "\xB0F";
+          fiveday[4].children[1].textContent = data.daily[4].wind_speed + "MPH";
+          fiveday[4].children[2].textContent = data.daily[4].humidity + "%";
+            //  for (i=0; i<fiveday.length; i++) {
+            //      fiveday.forEach(element =>
+            //     element.children[0].textContent = data.daily[0].temp.day);
+            //     //  fiveday.forEach(element =>
+            // //     console.log(element),
+            // //      temp.textContent = data.daily[i].temp.day);
+            //  }
+
         });
     });
 }
