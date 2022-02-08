44ada9b73f494fbae51f0ef488a6c95d90a47d4a
Michael Marlowe <marlowemich@gmail.com>
2022-02-08 11:58:52 -0500

    weather icons now displaying properly

--- script.js
+++ script.js
@@ -25,6 +25,8 @@ var day1temp = document.getElementById("temp1");
 var day1wind = document.getElementById("wind1");
 var day1hum = document.getElementById("hum1");
 var day1uvi = document.getElementById("uvi1");
+var day1icon = document.getElementById("icon1");
+var iconUrl = document.createElement("img");
 var fiveday = [
     document.getElementById("0"), 
     document.getElementById("1"), 
@@ -35,7 +37,6 @@ var fiveday = [
 var temp = document.getElementById("temp");
 var wind = document.getElementById("wind");
 var hum = document.getElementById("hum");
-
 btn.addEventListener("click", getAPI);
 
 function getAPI() {
@@ -55,6 +56,7 @@ function getAPI() {
     .then(function (data) {
      console.log(data)
       city.textContent = data.name + " " + moment().format("MM/DD/YYYY");
+      iconUrl.src = "http://openweathermap.org/img/wn/"+data.weather[0].icon+"@2x.png";
       var lat = data.coord.lat;
       var lon = data.coord.lon;
       var onecall = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + APIKey;
@@ -70,34 +72,35 @@ function getAPI() {
           day1hum.textContent = "Humidity: " + data.current.humidity + "%";
           day1uvi.textContent = "UV Index: " + data.current.uvi;
           day1wind.textContent = "Wind: " + data.current.wind_speed + "MPH";
+          day1icon.appendChild(iconUrl);
 
+              if (data.current.uvi>3){
+                //day1info.children[3].setAttribute("style", "background-color: green");
+              }
+                       
+// Category	UVI Range	Color
+// Low	0 – 2	Green
+// Moderate	3 – 5	Yellow
+// High	6 – 7	Orange
+// Very High	8 – 10	Red
+// Extreme	11 +	Purple
 
-          fiveday[0].children[0].textContent = "Temp: " + data.daily[0].temp.day + "\xB0F";
-          fiveday[0].children[1].textContent = "Wind: " + data.daily[0].wind_speed + "MPH";
-          fiveday[0].children[2].textContent = "Humidity: " + data.daily[0].humidity + "%";
+              for (var i=0; i<fiveday.length; i++) {
 
-          fiveday[1].children[0].textContent = "Temp: " + data.daily[1].temp.day + "\xB0F";
-          fiveday[1].children[1].textContent = "Wind: " + data.daily[1].wind_speed + "MPH";
-          fiveday[1].children[2].textContent = "Humidity: " + data.daily[1].humidity + "%";
+                var icon5 = document.createElement("img")
+                icon5.src = "http://openweathermap.org/img/wn/"+data.daily[i].weather[0].icon+"@2x.png";
+                fiveday[i].children[3].replaceWith(icon5);
 
-          fiveday[2].children[0].textContent = "Temp: " + data.daily[2].temp.day + "\xB0F";
-          fiveday[2].children[1].textContent = "Wind: " + data.daily[2].wind_speed + "MPH";
-          fiveday[2].children[2].textContent = "Humidity: " + data.daily[2].humidity + "%";
-          
-          fiveday[3].children[0].textContent = "Temp: " + data.daily[3].temp.day + "\xB0F";
-          fiveday[3].children[1].textContent = "Wind: " + data.daily[3].wind_speed + "MPH";
-          fiveday[3].children[2].textContent = "Humidity: " + data.daily[3].humidity + "%";
+                fiveday[i].children[0].textContent = "Temp: " + data.daily[i].temp.day + "\xB0F";
+                fiveday[i].children[1].textContent = "Wind: " + data.daily[i].wind_speed + "MPH";
+                fiveday[i].children[2].textContent = "Humidity: " + data.daily[i].humidity + "%";
+                 // fiveday.forEach(element =>
 
-          fiveday[4].children[0].textContent = "Temp: " + data.daily[4].temp.day + "\xB0F";
-          fiveday[4].children[1].textContent = "Wind: " + data.daily[4].wind_speed + "MPH";
-          fiveday[4].children[2].textContent = "Humidity: " + data.daily[4].humidity + "%";
-            //  for (i=0; i<fiveday.length; i++) {
-            //      fiveday.forEach(element =>
-            //     element.children[0].textContent = data.daily[0].temp.day);
-            //     //  fiveday.forEach(element =>
+                 //element.children[i].textContent = data.daily[i].temp.day;
+                 //  fiveday.forEach(element =>
             // //     console.log(element),
             // //      temp.textContent = data.daily[i].temp.day);
-            //  }
+              }
 
         });
     });
