b401ae33b91dadef1b38e093aee1c59470b02c84
Michael Marlowe <marlowemich@gmail.com>
2022-02-08 17:15:21 -0500

    everything is done except for the local storage functions

--- script.js
+++ script.js
@@ -18,7 +18,7 @@
 
 var APIKey = "4afcf2545fe74a0ac9054bd3660678f4";
 var input = document.getElementById("search");
-var btn = document.getElementById("searchbtn");
+var btn = document.getElementById("searchform");
 var city = document.getElementById("cityname");
 var day1info = document.getElementById("info1");
 var day1temp = document.getElementById("temp1");
@@ -28,38 +28,52 @@ var day1uvi = document.getElementById("uvi1");
 var day1icon = document.getElementById("icon1");
 var iconUrl = document.createElement("img");
 var fiveday = [
-    document.getElementById("0"), 
-    document.getElementById("1"), 
-    document.getElementById("2"), 
-    document.getElementById("3"), 
-    document.getElementById("4")
-]
+  document.getElementById("0"),
+  document.getElementById("1"),
+  document.getElementById("2"),
+  document.getElementById("3"),
+  document.getElementById("4"),
+];
 var temp = document.getElementById("temp");
 var wind = document.getElementById("wind");
 var hum = document.getElementById("hum");
-btn.addEventListener("click", getAPI);
 
-function getAPI() {
-  var weather = "https://api.openweathermap.org/data/2.5/weather?q=" + input.value + "&units=imperial&appid=" + APIKey;
+btn.addEventListener("submit", getAPI);
+
+//FETCHING OPENWEATHER API'S
+
+function getAPI(event) {
+  event.preventDefault();
+  var weather =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    input.value +
+    "&units=imperial&appid=" +
+    APIKey;
   fetch(weather)
     .then(function (response) {
       if (response.status === 404) {
         alert("No city in the world is even called that.");
       }
-      if (input.value === ""){
-        alert("Please specify a city. Like, literally anywhere in the world.")
-      }
-       else {
+      if (input.value === "") {
+        alert("Please specify a city. Like, literally anywhere in the world.");
+      } else {
         return response.json();
       }
     })
     .then(function (data) {
-     console.log(data)
+      console.log(data);
       city.textContent = data.name + " " + moment().format("MM/DD/YYYY");
-      iconUrl.src = "http://openweathermap.org/img/wn/"+data.weather[0].icon+"@2x.png";
+      iconUrl.src =
+        "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
       var lat = data.coord.lat;
       var lon = data.coord.lon;
-      var onecall = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + APIKey;
+      var onecall =
+        "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+        lat +
+        "&lon=" +
+        lon +
+        "&units=imperial&appid=" +
+        APIKey;
       return fetch(onecall)
         .then(function (response) {
           {
@@ -73,35 +87,86 @@ function getAPI() {
           day1uvi.textContent = "UV Index: " + data.current.uvi;
           day1wind.textContent = "Wind: " + data.current.wind_speed + "MPH";
           day1icon.appendChild(iconUrl);
+          //need to change color behind text of UVI try span over number of UVI as a seaprate element
+          var color = "";
+          if (data.current.uvi < 3) color = "green";
+            // day1info.children[3].setAttribute("style", "background-color: green");
+          else if (3 < data.current.uvi <= 5) color = "yellow";
+          // day1info.children[3].setAttribute("style", "background-color: yellow");
+          else if (5 < data.current.uvi <= 8) color = "orange";
+          // day1info.children[3].setAttribute("style", "background-color: orange");
+          else if (8 < data.current.uvi <= 11) color = "red";
+          // day1info.children[3].setAttribute("style", "background-color: red");
+          else if (11 < data.current.uvi) color = "purple";
+            //day1info.children[3].setAttribute("style", "background-color: purple");
+          day1info.children[3].setAttribute(
+            "style",
+            `background-color: ${color}`
+          );
+          //PLACING PERTINENT INFO IN 5 DAY FORECAST
 
-              if (data.current.uvi>3){
-                //day1info.children[3].setAttribute("style", "background-color: green");
-              }
-                       
-// Category	UVI Range	Color
-// Low	0 – 2	Green
-// Moderate	3 – 5	Yellow
-// High	6 – 7	Orange
-// Very High	8 – 10	Red
-// Extreme	11 +	Purple
-
-              for (var i=0; i<fiveday.length; i++) {
-
-                var icon5 = document.createElement("img")
-                icon5.src = "http://openweathermap.org/img/wn/"+data.daily[i].weather[0].icon+"@2x.png";
-                fiveday[i].children[3].replaceWith(icon5);
-
-                fiveday[i].children[0].textContent = "Temp: " + data.daily[i].temp.day + "\xB0F";
-                fiveday[i].children[1].textContent = "Wind: " + data.daily[i].wind_speed + "MPH";
-                fiveday[i].children[2].textContent = "Humidity: " + data.daily[i].humidity + "%";
-                 // fiveday.forEach(element =>
-
-                 //element.children[i].textContent = data.daily[i].temp.day;
-                 //  fiveday.forEach(element =>
-            // //     console.log(element),
-            // //      temp.textContent = data.daily[i].temp.day);
-              }
-
+          for (var i = 0; i < fiveday.length; i++) {
+            var icon5 = document.createElement("img");
+            icon5.src =
+              "http://openweathermap.org/img/wn/" +
+              data.daily[i].weather[0].icon +
+              "@2x.png";
+            fiveday[i].children[3].replaceWith(icon5);
+            fiveday[i].children[0].textContent =
+              "Temp: " + data.daily[i].temp.day + "\xB0F";
+            fiveday[i].children[1].textContent =
+              "Wind: " + data.daily[i].wind_speed + "MPH";
+            fiveday[i].children[2].textContent =
+              "Humidity: " + data.daily[i].humidity + "%";
+          }
         });
     });
 }
+
+// saveLocal(){
+
+// }
+
+// btn.addEventListener("click", function(event){
+//   event.preventDefault();
+//   saveLocal();
+// });
+
+// function init(){
+//   renderLastRegistered();
+// }
+// init();
+
+// function renderLastRegistered() {
+//   var items = JSON.parse(localStorage.getItem("input"));
+//   for (i = 0; i<changers.length; i++){
+//     changers[i].val(items[i])
+//   }
+//   console.log(items);
+// };
+
+// function savelocal(){
+//  var input = [
+//    $('textarea[id="9"]').val(),
+//    $('textarea[id="10"]').val(),
+//    $('textarea[id="11"]').val(),
+//    $('textarea[id="12"]').val(),
+//    $('textarea[id="13"]').val(),
+//    $('textarea[id="14"]').val(),
+//    $('textarea[id="15"]').val(),
+//    $('textarea[id="16"]').val(),
+//    $('textarea[id="17"]').val(),
+//  ];
+// localStorage.setItem("input", JSON.stringify(input));
+// }
+
+// var button = $(".saveBtn");
+// button.on("click", function (event) {
+//  event.preventDefault();
+//  savelocal();
+// });
+
+// function init(){
+//  renderLastRegistered();
+// }
+// init();
